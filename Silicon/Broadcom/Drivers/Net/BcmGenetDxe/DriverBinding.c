54b61339202a29c347b438d46cb9f0a07437cd8d
Samer El-Haj-Mahmoud <samer.el-haj-mahmoud@arm.com>
2020-04-18 11:29:25 -0400

    Driver/BcmGenetDxe: Add DriverBinding function headers
    Add function headers for DriverBinding
    
    Signed-off-by: Samer El-Haj-Mahmoud <samer@elhajmahmoud.com>

--- Silicon/Broadcom/Drivers/Net/BcmGenetDxe/DriverBinding.c
+++ Silicon/Broadcom/Drivers/Net/BcmGenetDxe/DriverBinding.c
@@ -78,6 +78,23 @@ STATIC GENET_DEVICE_PATH mDevicePath = {
   }
 };
 
+/**
+  Tests to see if this driver supports a given controller.
+
+  @param  This[in]                 A pointer to the EFI_DRIVER_BINDING_PROTOCOL
+                                   instance.
+  @param  ControllerHandle[in]     The handle of the controller to test.
+  @param  RemainingDevicePath[in]  The remaining device path.
+                                   (Ignored - this is not a bus driver.)
+
+  @retval EFI_SUCCESS              The driver supports this controller.
+  @retval EFI_ALREADY_STARTED      The device specified by ControllerHandle is
+                                   already being managed by the driver specified
+                                   by This.
+  @retval EFI_UNSUPPORTED          The device specified by ControllerHandle is
+                                   not supported by the driver specified by This.
+
+**/
 EFI_STATUS
 EFIAPI
 GenetDriverBindingSupported (
@@ -104,6 +121,25 @@ GenetDriverBindingSupported (
   return EFI_SUCCESS;
 }
 
+
+/**
+  Starts a device controller or a bus controller.
+
+  @param[in]  This                 A pointer to the EFI_DRIVER_BINDING_PROTOCOL
+                                   instance.
+  @param[in]  ControllerHandle     The handle of the device to start. This
+                                   handle must support a protocol interface that
+                                   supplies an I/O abstraction to the driver.
+  @param[in]  RemainingDevicePath  The remaining portion of the device path.
+                                   (Ignored - this is not a bus driver.)
+
+  @retval EFI_SUCCESS              The device was started.
+  @retval EFI_DEVICE_ERROR         The device could not be started due to a
+                                   device error.
+  @retval EFI_OUT_OF_RESOURCES     The request could not be completed due to a
+                                   lack of resources.
+
+**/
 EFI_STATUS
 EFIAPI
 GenetDriverBindingStart (
@@ -199,6 +235,25 @@ GenetDriverBindingStart (
   return Status;
 }
 
+
+/**
+  Stops a device controller or a bus controller.
+
+  @param[in]  This              A pointer to the EFI_DRIVER_BINDING_PROTOCOL
+                                instance.
+  @param[in]  ControllerHandle  A handle to the device being stopped. The handle
+                                must support a bus specific I/O protocol for the
+                                driver to use to stop the device.
+  @param[in]  NumberOfChildren  The number of child device handles in
+                                ChildHandleBuffer.
+  @param[in]  ChildHandleBuffer An array of child handles to be freed. May be
+                                NULL if NumberOfChildren is 0.
+
+  @retval EFI_SUCCESS           The device was stopped.
+  @retval EFI_DEVICE_ERROR      The device could not be stopped due to a device
+                                error.
+
+**/
 EFI_STATUS
 EFIAPI
 GenetDriverBindingStop (
@@ -245,6 +300,18 @@ GenetDriverBindingStop (
   return EFI_SUCCESS;
 }
 
+
+/**
+  The entry point of Netsec UEFI Driver.
+
+  @param  ImageHandle                The image handle of the UEFI Driver.
+  @param  SystemTable                A pointer to the EFI System Table.
+
+  @retval  EFI_SUCCESS               The Driver or UEFI Driver exited normally.
+  @retval  EFI_INCOMPATIBLE_VERSION  _gUefiDriverRevision is greater than
+                                     SystemTable->Hdr.Revision.
+
+**/
 EFI_STATUS
 EFIAPI
 GenetEntryPoint (
